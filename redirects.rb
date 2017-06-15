de71777815c34ba713ffcfe27688b730be009b8e
Paul Spinrad <pspinrad@pivotal.io>
2017-06-15 12:04:17 -0700

    move partners/datastax/ to partners/datastax-service/broker

--- redirects.rb
+++ redirects.rb
@@ -20,6 +20,7 @@ r301 %r{/partners/rlec/(.*)}, '/partners/rle-service-broker/index.html'
 # Partners redirects
 r301 %r{/partners/appmon/(.*)}, '/partners/dynatrace/index.html'
 r301 %r{/partners/ruxit/(.*)}, '/partners/dynatrace/index.html'
+r301 %r{/partners/datastax/(.*)}, '/partners/datastax-service-broker/index.html'
 
 # Is this current as of Q1 2017?
 r301 %r{/partners/cjoc/(.*)}, 'https://docs.pivotal.io'
